33cf38cfc678c1aa5e6fbbe49bdae56e894bdd60
Carol-seven <542046605@qq.com>
2024-10-12 14:30:28 -0400

    rm diag opt

--- R/fcstat_method.R
+++ R/fcstat_method.R
@@ -26,44 +26,22 @@
 #' @param pkgopt A character string specifying the package option to use. The available
 #' options depend on the selected method: \enumerate{
 #' \item For \code{method = "glasso"}: \itemize{
-#' \item "ADMMsigma_diagtrue": the function from \code{\link[ADMMsigma]{ADMMsigma}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are penalized.
-#' \item "ADMMsigma_diagfalse": the function from \code{\link[ADMMsigma]{ADMMsigma}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are not
-#' penalized.
-#' \item "CovTools": the function from \code{\link[CovTools]{PreEst.glasso}}.
-#' \item "CVglasso_diagtrue": the function from \code{\link[CVglasso]{CVglasso}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are penalized.
-#' \item "CVglasso_diagfalse": the function from \code{\link[CVglasso]{CVglasso}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are not
-#' penalized.
-#' \item "glasso_cold_diagtrue": the function from \code{\link[glasso]{glasso}}, and the
-#' diagonal elements of the estimated precision matrix are penalized.
-#' \item "glasso_cold_diagfalse": the function from \code{\link[glasso]{glasso}}, and the
-#' diagonal elements of the estimated precision matrix are not penalized.
-#' \item "glasso_warm_diagtrue": the function from \code{\link[glasso]{glasso}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are penalized.
-#' \item "glasso_warm_diagfalse": the function from \code{\link[glasso]{glasso}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are not
-#' penalized.
-#' \item "GLassoElnetFast_cold_diagtrue": the function from
-#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet}, and the diagonal
-#' elements of the estimated precision matrix are penalized.
-#' \item "GLassoElnetFast_cold_diagfalse": the function from
-#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet}, and the diagonal
-#' elements of the estimated precision matrix are not penalized.
-#' \item "GLassoElnetFast_warm_diagtrue": the function from
-#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet} with warm-starts, and
-#' the diagonal elements of the estimated precision matrix are penalized.
-#' \item "GLassoElnetFast_warm_diagfalse": the function from
-#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet} with warm-starts, and
-#' the diagonal elements of the estimated precision matrix are not penalized.
-#' \item "glassoFast_cold": the function from \code{\link[glassoFast]{glassoFast}}, and
-#' the diagonal elements of the estimated precision matrix are penalized.
+#' \item "ADMMsigma": the function from \code{\link[ADMMsigma]{ADMMsigma}} with
+#' warm-starts.
+#' \item "CovTools": the function from \code{\link[CovTools]{PreEst.glasso}} with
+#' cold-starts.
+#' \item "CVglasso": the function from \code{\link[CVglasso]{CVglasso}} with warm-starts.
+#' \item "glasso_cold": the function from \code{\link[glasso]{glasso}} with cold-starts.
+#' \item "glasso_warm": the function from \code{\link[glasso]{glasso}} with warm-starts.
+#' \item "GLassoElnetFast_cold": the function from
+#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet} with cold-starts.
+#' \item "GLassoElnetFast_warm": the function from
+#' \href{https://github.com/TobiasRuckstuhl/GLassoElnetFast}{gelnet} with warm-starts.
+#' \item "glassoFast_cold": the function from \code{\link[glassoFast]{glassoFast}} with
+#' cold-starts.
 #' \item "glassoFast_warm": the function from \code{\link[glassoFast]{glassoFast}} with
-#' warm-starts, and the diagonal elements of the estimated precision matrix are penalized.
-#' \item "huge": the function from \code{\link[huge]{huge.glasso}}, and the diagonal
-#' elements of the estimated precision matrix are penalized.
+#' warm-starts.
+#' \item "huge": the function from \code{\link[huge]{huge.glasso}} with cold-starts.
 #' }
 #' \item For \code{method = "ridge"}: \itemize{
 #' \item "ADMMsigma": the function from \code{\link[ADMMsigma]{RIDGEsigma}}.
@@ -112,35 +90,21 @@ fcstat_method <- function(method, X = NULL, S = NULL,
                           lambda = NULL, gamma = NULL,
                           pkgopt = NULL) {
   if (method == "glasso") {
-    if (pkgopt == "ADMMsigma_diagtrue") {
+    if (pkgopt == "ADMMsigma") {
       hatOmega <- ADMMsigma::ADMMsigma(S = S, lam = lambda, alpha = 1, diagonal = TRUE)$Z
-    } else if (pkgopt == "ADMMsigma_diagfalse") {
-      hatOmega <- ADMMsigma::ADMMsigma(S = S, lam = lambda, alpha = 1, diagonal = FALSE)$Z
     } else if (pkgopt == "CovTools") {
       hatOmega <- CovTools::PreEst.glasso(X = X, method = list(type = "fixed", param = lambda))$C
-    } else if (pkgopt == "CVglasso_diagtrue") {
+    } else if (pkgopt == "CVglasso") {
       hatOmega <- CVglasso::CVglasso(S = S, lam = lambda, diagonal = TRUE)$Omega
-    } else if (pkgopt == "CVglasso_diagfalse") {
-      hatOmega <- CVglasso::CVglasso(S = S, lam = lambda, diagonal = FALSE)$Omega
-    } else if (pkgopt == "glasso_cold_diagtrue") {
+    } else if (pkgopt == "glasso_cold") {
       hatOmega <- glasso::glasso(s = S, rho = lambda, penalize.diagonal = TRUE, start = "cold")$wi
-    } else if (pkgopt == "glasso_cold_diagfalse") {
-      hatOmega <- glasso::glasso(s = S, rho = lambda, penalize.diagonal = FALSE, start = "cold")$wi
-    } else if (pkgopt == "glasso_warm_diagtrue") {
+    } else if (pkgopt == "glasso_warm") {
       hatOmega <- glasso::glasso(s = S, rho = lambda, penalize.diagonal = TRUE, start = "warm",
                                  w.init = S + lambda, wi.init = diag(ncol(S)))$wi
-    } else if (pkgopt == "glasso_warm_diagfalse") {
-      hatOmega <- glasso::glasso(s = S, rho = lambda, penalize.diagonal = FALSE, start = "warm",
-                                 w.init = S + lambda, wi.init = diag(ncol(S)))$wi
-    } else if (pkgopt == "GLassoElnetFast_cold_diagtrue") {
+    } else if (pkgopt == "GLassoElnetFast_cold") {
       hatOmega <- GLassoElnetFast::gelnet(S = S, lambda = lambda, alpha = 1, penalize.diagonal = TRUE)$Theta
-    } else if (pkgopt == "GLassoElnetFast_cold_diagfalse") {
-      hatOmega <- GLassoElnetFast::gelnet(S = S, lambda = lambda, alpha = 1, penalize.diagonal = FALSE)$Theta
-    } else if (pkgopt == "GLassoElnetFast_warm_diagtrue") {
+    } else if (pkgopt == "GLassoElnetFast_warm") {
       hatOmega <- GLassoElnetFast::gelnet(S = S, lambda = lambda, alpha = 1, penalize.diagonal = TRUE,
-                                          W = S + lambda, Theta = diag(ncol(S)))$Theta
-    } else if (pkgopt == "GLassoElnetFast_warm_diagfalse") {
-      hatOmega <- GLassoElnetFast::gelnet(S = S, lambda = lambda, alpha = 1, penalize.diagonal = FALSE,
                                           W = S + lambda, Theta = diag(ncol(S)))$Theta
     } else if (pkgopt == "glassoFast_cold") {
       hatOmega <- glassoFast::glassoFast(S = S, rho = lambda, start = "cold")$wi
