ed35285c97b1f924a986aa6dd71eca9a4a98dd9e
Carol-seven <542046605@qq.com>
2026-02-18 20:05:18 -0500

    ncv_deriv(): rename function; update documentation

--- R/ncv_deriv.R
+++ R/ncv_deriv.R
@@ -1,23 +1,26 @@
-#' Derivative of the non-convex regularization penalty
+#' Derivative of a Non-Convex Penalty
 #'
 #' @description
-#' Compute the derivative of the specified non-convex regularization penalty.
+#' Compute the derivative of a specified non-convex regularization penalty.
 #'
 #' @param penalty A character string specifying the non-convex penalty to use.
 #' Available options include: \enumerate{
-#' \item "adapt": adaptive lasso \insertCite{zou2006adaptive,fan2009network}{fcstat}.
-#' \item "atan": arctangent type penalty \insertCite{wang2016variable}{fcstat}.
-#' \item "exp": exponential type penalty \insertCite{wang2018variable}{fcstat}.
-#' \item "mcp": minimax concave penalty \insertCite{zou2006adaptive}{fcstat}.
-#' \item "scad": smoothly clipped absolute deviation \insertCite{fan2001variable,fan2009network}{fcstat}.
+#' \item "adapt": Adaptive lasso
+#' \insertCite{zou2006adaptive,fan2009network}{spice}.
+#' \item "atan": Arctangent type penalty \insertCite{wang2016variable}{spice}.
+#' \item "exp": Exponential type penalty \insertCite{wang2018variable}{spice}.
+#' \item "mcp": Minimax concave penalty \insertCite{zou2006adaptive}{spice}.
+#' \item "scad": Smoothly clipped absolute deviation
+#' \insertCite{fan2001variable,fan2009network}{spice}.
 #' }
 #'
 #' @param Omega The precision matrix.
 #'
-#' @param lambda A scalar specifying the regularization parameter.
+#' @param lambda A non-negative numeric value specifying the regularization
+#' parameter.
 #'
-#' @param gamma A scalar specifying the hyperparameter for the penalty function.
-#' The defaults are: \enumerate{
+#' @param gamma A numeric value specifying the additional parameter for
+#' the penalty function. The defaults are: \enumerate{
 #' \item "adapt": 0.5
 #' \item "atan": 0.005
 #' \item "exp": 0.01
@@ -34,33 +37,22 @@
 #'
 #' @export
 
-deriv <- function(penalty, Omega, lambda, gamma) {
+ncv_deriv <- function(penalty, Omega, lambda, gamma) {
+  if (missing(gamma)) {
+    gamma <- switch(penalty, "adapt" = 0.5, "atan" = 0.005,
+                    "exp"  = 0.01, "mcp"  = 3, "scad" = 3.7, NA)
+  }
   if (penalty == "adapt") {
-    if (missing(gamma)) {
-      gamma <- 0.5
-    }
     weight <- abs(Omega)^(-gamma)
     weight[is.infinite(weight)] <- 9999999999
     lambda_mat <- lambda*weight
   } else if (penalty == "atan") {
-    if (missing(gamma)) {
-      gamma <- 0.005
-    }
     lambda_mat <- lambda * gamma * (gamma + 2/pi) / (gamma^2 + Omega^2)
   } else if (penalty == "exp") {
-    if (missing(gamma)) {
-      gamma <- 0.01
-    }
     lambda_mat <- (lambda/gamma) * exp(-abs(Omega)/gamma)
   } else if (penalty == "mcp") {
-    if (missing(gamma)) {
-      gamma <- 3
-    }
     lambda_mat <- (lambda - abs(Omega)/gamma) * (abs(Omega) <= gamma*lambda)
   } else if (penalty == "scad") {
-    if (missing(gamma)) {
-      gamma <- 3.7
-    }
     lambda_mat <- lambda * (abs(Omega) <= lambda) +
       pmax(gamma*lambda - abs(Omega), 0) / (gamma-1) * (abs(Omega) > lambda)
   }
